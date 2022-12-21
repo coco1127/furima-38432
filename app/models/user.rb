870322e342a15c3683c8503c8fbbf66f4f088ea5
erika <erika0917.em@gmail.com>
2022-12-22 06:33:15 +0900

    新規登録/本人情報の確認

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, presence: true
+  validates :nickname, presence: true
   validates :email, presence: true
-  validates :encrypted_password,:password,:password_confirmation,length:{minimum:7},format:{with: /(?=.[a-zA-Z])(?=.\d)[a-zA-Z\d]{7,}/}
+  validates :password,:password_confirmation, presence: true, length: { maximum: 7 }
 end
