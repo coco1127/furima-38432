f7bfb46723c2063074719139b6cabf8fa1399811
erika <erika0917.em@gmail.com>
2022-12-25 14:21:01 +0900

    単体テスト修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -106,7 +106,7 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
-      it 'passwordは英数字混合でないと登録できない' do
+      it 'パスワードが半角数字のみでは登録できない' do
         @user.password = '000000'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password",
