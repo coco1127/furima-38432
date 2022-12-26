ab69e7b2272196522f610faf4ff2dcf3914233d8
erika <erika0917.em@gmail.com>
2022-12-26 20:22:11 +0900

    投稿画面へのルーティング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,26 @@
 class ItemsController < ApplicationController
   def index
     @items = Item.order("created_at DESC")
+    
   end
 
-  #def create
-  #end
+  def new
+    @items = Item.new
+  end
+
+  def create
+    @items = Item.new(item_params)
+    if @items .save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:introduction,:price,:item_condition_id,:preparation_day_id,:postage_payer_id,:category_id,:trading_status_id)
+  end
+
 end
