efcf84cf6753f0631de417dbc7e2a73f8568681c
erika <erika0917.em@gmail.com>
2022-12-29 20:59:39 +0900

    データベースへの保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,22 +5,20 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @items = Item.new
+    @item = Item.new
   end
 
   def create
-    @items = Item.new(item_params)
-    if @items .save
-      redirect_to root_path
-    else
-      render :new
-    end
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to root_path
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name,:introduction,:price,:item_condition_id,:preparation_day_id,:postage_payer_id,:category_id,:trading_status_id)
+    params.require(:item).permit(:name,:introduction,:price,:item_condition_id,:preparation_day_id,:postage_payer_id,:category_id,:trading_status_id,:image).merge(user_id: current_user.id)
+
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,14 +9,14 @@ has_one_attached :image
 has_one :purchase_history
 belongs_to :user
 
-validates :item_condition, numericality: { other_than: 1 , message: "can't be blank"} 
-validates :preparation_day, numericality:  { other_than: 1 , message: "can't be blank"} 
-validates :postage_payer, numericality:  { other_than: 1 , message: "can't be blank"} 
-validates :category, numericality:  { other_than: 1 , message: "can't be blank"} 
-validates :trading_status, numericality:  { other_than: 1 , message: "can't be blank"} 
+validates :item_condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
+validates :preparation_day_id, numericality:  { other_than: 1 , message: "can't be blank"} 
+validates :postage_payer_id, numericality:  { other_than: 1 , message: "can't be blank"} 
+validates :category_id, numericality:  { other_than: 1 , message: "can't be blank"} 
+validates :trading_status_id, numericality:  { other_than: 1 , message: "can't be blank"} 
 
-validates :name,            :string,	       presence: true
-validates :introduction,    :text,           presence: true
-validates :price,           :integer,        presence: true
-     
+validates :name,                   presence: true
+validates :introduction,           presence: true
+validates :price,                  presence: true
+validates :image,                  presence: true
 end
