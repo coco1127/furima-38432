2ef706b2f93492e0a00b01fb32cd88d30c7bad39
erika <erika0917.em@gmail.com>
2023-01-07 06:42:38 +0900

    新規投稿商品表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
   def index
-    #@items = Item.order("created_at DESC")       一覧機能
+    @items = Item.all.order("created_at DESC")       
     
   end
 
