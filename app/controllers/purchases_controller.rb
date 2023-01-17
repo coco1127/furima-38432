042aefb40cb539352f9ed230e0c627ec08195996
erika <erika0917.em@gmail.com>
2023-01-17 18:46:11 +0900

    Form情報保存

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,24 +1,28 @@
 class PurchasesController < ApplicationController
   def index
    @item = Item.find(params[:item_id])
-   @shipping_address = ShippingAddress.new
+   @purchase_shipping = PurchaseShipping.new
+                 
     
   end
 
   def create
     @item = Item.find(params[:item_id])
-    @shipping_address = ShippingAddress.new
-    if @shipping_address.valid?
-      @shipping_address.save
+    @purchase_shipping = PurchaseShipping.new(purchase_params)
+    if @purchase_shipping.valid?
+      @purchase_shipping.save
       redirect_to root_path 
     else
       render :index
     end
   end
 
+
   private
 
   def purchase_params
-    params.require(:shipping_address).permit(:post_code, :city, :house_number, :building_name, :phone_number,:trading_status_id, :purchase_history).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:purchase_shipping).permit(:user_id, :item_id, :post_code, :city, :house_number, :building_name, :phone_number,:trading_status_id,:purchase_history_id).merge(user_id: current_user.id, item_id: @item.id)
+
+
   end
 end
--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -1,9 +1,25 @@
-class PurchaseShipping < ApplicationRecord
+class PurchaseShipping 
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :post_code, :city, :house_number, :building_name, :phone_number,:trading_status_id,:purchase_history_id
-  # ここにバリデーションの処理を書く
+  
+
+  with_options presence: true do
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :house_number
+    validates :phone_number, format: {with: /\A\d{10}\z|\A\d{11}\z/ , message: "is invalid."}
+    #validates :purchase_history_id
+    validates :user_id
+    validates :item_id
+    #validates :token
+  end
+    validates :trading_status_id, numericality:  { other_than: 1, message: "can't be blank" }
+             
 
   def save
-    # 各テーブルにデータを保存する処理を書く
+    purchase_history = PurchaseHistory.create(item_id: item_id, user_id: user_id)
+    ShippingAddress.create(post_code: post_code, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, trading_status_id: trading_status_id, purchase_history_id: purchase_history.id )
   end
 end
+
+
