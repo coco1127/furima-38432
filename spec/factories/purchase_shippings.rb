b6453bea38c96d2025b7618da5ca10540491638e
erika <erika0917.em@gmail.com>
2023-01-18 15:23:22 +0900

    purchase_shippingテスト

--- spec/factories/purchase_shippings.rb
+++ spec/factories/purchase_shippings.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :purchase_shipping do
-    
+post_code         { '123-4567' }
+city              { '東京都' }
+house_number      { '1-1' }
+building_name     { '東京ハイツ' }  
+phone_number       {'09000000000'}
+trading_status_id   { 6 }
+             
+
   end
 end
+
+
--- spec/models/purchase_shipping_spec.rb
+++ spec/models/purchase_shipping_spec.rb
@@ -1,5 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseShipping, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  describe '購入情報の保存' do
+    before do
+      user = FactoryBot.create(:user)
+      item = FactoryBot.create(:item)
+      @purchase_shipping = FactoryBot.build(:purchase_shipping, user_id: user.id, item_id: item.id)
+    end
+    context '内容に問題ない場合' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@purchase_shipping).to be_valid
+      end
+      it 'building_nameは空でも保存できること' do
+        @purchase_shipping.building_name = ''
+        expect(@purchase_shipping).to be_valid
+    end
+    end
+
+    context '内容に問題がある場合' do
+      it '郵便番号が空だと保存できないこと' do
+        @purchase_shipping.post_code = ''
+        @purchase_shipping.valid?
+        expect(@purchase_shipping.errors.full_messages).to include("Post code can't be blank")
+      end
+      it '郵便番号が半角のハイフンを含んだ正しい形式でないと保存できないこと' do
+        @purchase_shipping.post_code = '1234567'
+        @purchase_shipping.valid?
+        expect(@purchase_shipping.errors.full_messages).to include('Post code is invalid. Include hyphen(-)')
+      end
+        it '都道府県が未選択では登録できないこと' do
+          @purchase_shipping.trading_status_id = 1
+          @purchase_shipping.valid?
+          expect(@purchase_shipping.errors.full_messages).to include("Trading status can't be blank")
+      end
+      it '市町村が空だと保存できないこと' do
+        @purchase_shipping.city = nil
+        @purchase_shipping.valid?
+        expect(@purchase_shipping.errors.full_messages).to include("City can't be blank")
+      end
+      it '番地が空だと保存できないこと' do
+        @purchase_shipping.house_number = nil
+        @purchase_shipping.valid?
+        expect(@purchase_shipping.errors.full_messages).to include("House number can't be blank")
+      end
+      it '電話番号が空だと保存できないこと' do
+        @purchase_shipping.phone_number = nil
+        @purchase_shipping.valid?
+        expect(@purchase_shipping.errors.full_messages).to include("Phone number can't be blank")
+      end
+        it '電話番号が全角数字だと保存できないこと' do
+          @purchase_shipping.phone_number = '２００００００００００'
+          @purchase_shipping.valid?
+          expect(@purchase_shipping.errors.full_messages).to include('Phone number is invalid.')
+      end
+    end
+    end
+  end
+
