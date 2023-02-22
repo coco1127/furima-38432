8881e1c19480f6ef693e87cac870b9c22f08d4bd
erika <erika0917.em@gmail.com>
2023-02-22 21:24:28 +0900

    モデルの日本語化

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,20 +9,20 @@ class Item < ApplicationRecord
   has_one     :purchase_history
   belongs_to  :user
 
-  validates :item_condition_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :preparation_day_id, numericality:  { other_than: 1, message: "an't be blank" }
-  validates :postage_payer_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :trading_status_id, numericality:  { other_than: 1, message: "can't be blank" }
+  validates :item_condition_id, numericality: { other_than: 1, message: 'を選択して下さい'}
+  validates :preparation_day_id, numericality:  { other_than: 1,  message: 'を選択して下さい'}
+  validates :postage_payer_id, numericality: { other_than: 1,  message: 'を選択して下さい'}
+  validates :category_id, numericality: { other_than: 1,  message: 'を選択して下さい' }
+  validates :trading_status_id, numericality:  { other_than: 1, message: 'を選択して下さい'}
 
   validates :name,                   presence: true
   validates :introduction,           presence: true
-  validates :price,                  format: { with: /\A[0-9]+\z/, message: 'Price is invalid. Input half-width characters' }
+  validates :price,                  format: { with: /\A[0-9]+\z/, message: 'は半角で入力して下さい' }
   validates :price,
             numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
-                            message: 'Out of setting range' }
+                            message: 'は範囲内で設定して下さい' }
 
-  validates :image,                  presence: true
+  validates :image,                  presence: true 
   validates :item_condition,         presence: true
   validates :preparation_day,        presence: true
   validates :postage_payer,          presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,12 +11,10 @@ class User < ApplicationRecord
   validates :birth_date, presence: true
   validates :password,
             format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i,
-                      message: 'Password is invalid. Include both letters and numbers' }
+                      message: 'と同じにものを入力して下さい' }
 
   with_options presence: true do
-    validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid.
-                               Input full-width characters." }
-    validates :family_name_kana, :first_name_kana, format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/, message: "is invalid.
-                                Input full-width characters." }
+    validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角で入力して下さい" }
+    validates :family_name_kana, :first_name_kana, format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/, message: "は全角で入力して下さい" }
   end
 end
